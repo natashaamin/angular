e2c17328dec20cfd9481942cf92ec3f1a929f829
natasha.s.binti.amin <natasha.s.binti.amin@accenture.com>
2020-10-06 14:31:02 +0800

    Alert: Adding service to the component

--- src/alert/alert.module.ts
+++ src/alert/alert.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { AlertComponent } from './alert.component'
 import { ContainerComponent, ContainerModule } from 'ktb-ui-components/container'
+import { AlertService } from './alert.service'
 
 @NgModule({
   declarations: [AlertComponent],
   imports: [CommonModule, ContainerModule],
   exports: [AlertComponent, ContainerComponent],
-  providers: []
+  providers: [AlertService]
 })
 export class AlertModule {}
--- src/alert/alert.stories.ts
+++ src/alert/alert.stories.ts
@@ -1,25 +1,64 @@
-import { Meta, moduleMetadata } from '@storybook/angular'
-import { AlertComponent, AlertModule, AlertService } from 'ktb-ui-components/alert'
+import { Component, ViewEncapsulation } from '@angular/core'
+import { moduleMetadata, storiesOf } from '@storybook/angular'
+import { AlertModule, AlertService } from 'ktb-ui-components/alert'
 
-export default {
-  title: 'Alert',
-  component: AlertComponent,
-  decorators: [
-    moduleMetadata({
-      imports: [AlertModule],
-      providers: [AlertService]
-    })
-  ]
-} as Meta
-
-export const Overview = () => ({
-  component: [AlertComponent],
+@Component({
+  selector: 'alert-button',
   template: `
-            <h2>Success Alert Notification</h2>
-            <ktbAlert></ktbAlert>
+    <ktbAlert></ktbAlert>
 
-            <div>
-            <button class="btn btn-success m-1" (click)="_alertService.success('ALERT: These are scam calls targeting customers to make bank transfers. DO NOT disclose any account details, USER Id, PINS or SMS OTPs. Learn more.', options)">Success</button>
-            </div>
-            `
+    <div>
+      <button
+        class="btn btn-success m-1"
+        (click)="
+          _alertService.success(
+            'ALERT: These are scam calls targeting customers to make bank transfers. DO NOT disclose any account details, USER Id, PINS or SMS OTPs. Learn more.',
+            options
+          )
+        "
+      >
+        Success
+      </button>
+      <button
+        class="btn btn-danger m-1"
+        (click)="
+          _alertService.error(
+            'ALERT: These are scam calls targeting customers to make bank transfers. DO NOT disclose any account details, USER Id, PINS or SMS OTPs. Learn more.',
+            options
+          )
+        "
+      >
+        Error
+      </button>
+      <button
+        class="btn btn-warning m-1"
+        (click)="
+          _alertService.warn(
+            'ALERT: These are scam calls targeting customers to make bank transfers. DO NOT disclose any account details, USER Id, PINS or SMS OTPs. Learn more.',
+            options
+          )
+        "
+      >
+        Warning
+      </button>
+    </div>
+  `,
+  providers: [AlertService]
 })
+class AlertButtonComponent {
+  constructor(public _alertService: AlertService) {}
+}
+
+storiesOf('Alert Notification', module)
+  .addDecorator(
+    moduleMetadata({
+      declarations: [AlertButtonComponent],
+      imports: [AlertModule]
+    })
+  )
+  .add('Alert Notification', () => ({
+    template: `
+            <alert-button></alert-button>
+            `,
+    encapsulation: ViewEncapsulation.None
+  }))
